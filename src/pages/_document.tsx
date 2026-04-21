49083bc9e6e0de482a70282fdff1fb62c6adf8ca
jeanbeanie <msjeanneramos@gmail.com>
2026-04-21 10:41:20 -0600

    write script to fetch persisted theme (dark/light) from local storage, update css styles to use tailwind classes and class based dark mode using :root.dark

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+          <Head>
+              <script
+                  dangerouslySetInnerHTML={{
+                      __html: `
+                        (function () {
+                          try {
+                            var stored = localStorage.getItem("theme"); // "dark" | "light" | null
+                            var theme = stored ? stored : "dark";       // <-- default to dark
+                            if (theme === "dark") document.documentElement.classList.add("dark");
+                            else document.documentElement.classList.remove("dark");
+                          } catch (e) {}
+                        })();`,
+                  }}
+              />
+          </Head>
       <body className="antialiased antialiased bg-paper text-slate dark:bg-ink dark:text-white">
         <Main />
         <NextScript />
